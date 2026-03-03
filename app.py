c4572d7253ee521699b7993591125437ae16ee8c
Akarsh_Agarwal_6688 <agarwalakarsh6688@gmail.com>
2026-03-04 01:26:00 +0530

    Fix CORS for frontend

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from sniffer import generate_fake_packet
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def home():
